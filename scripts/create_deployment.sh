eb207cfc691e36898eae0e5a9012fdeb1b3880ef
David Schepis <davidschepis@gmail.com>
2022-05-21 22:26:57 -0700

    added docker/kubernetes menu

--- scripts/create_deployment.sh
+++ scripts/create_deployment.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-cd ../kube-manifests
-kubectl apply -f deployment.yaml
-kubectl apply -f load-balancer.yaml
-kubectl apply -f volume.yaml
+cd ..
+ls
+#kubectl apply -f deployment.yaml
+#kubectl apply -f load-balancer.yaml
+#kubectl apply -f volume.yaml
---
+++ scripts/docker_local_create.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -it --name notetaker -d -p 3001:3001 davidsdeveloper/note-taker:v$1
